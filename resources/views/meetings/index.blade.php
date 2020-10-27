1529cd9442b43358a55729bf84a437c1aaecab8b
Daniel Berry <daniel@jstreetdigital.com>
2020-10-27 10:49:51 -0500

    update existing meeting views to match theme
    Signed-off-by: Daniel Berry <daniel@jstreetdigital.com>

--- resources/views/meetings/index.blade.php
+++ resources/views/meetings/index.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Meetings</h1>
+    <div class="container">
+        <div class="row mb-4">
+            <div class="col">
+                <h1>Meetings</h1>
+            </div>
+        </div>
 
-    @foreach($meetings as $meeting)
-    <div>
-        <a href="/meetings/{{$meeting->id}}">{{$meeting->title}}</a>
+        <div class="row">
+            <div class="col-md-12">
+                @foreach($meetings as $meeting)
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <a href="/meetings/{{$meeting->id}}">{{$meeting->title}}</a>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
     </div>
-    @endforeach
 @endsection
--- resources/views/meetings/show.blade.php
+++ resources/views/meetings/show.blade.php
@@ -1,16 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/meetings">&lt; Back</a>
+    <div class="container">
+        <div class="row mb-2">
+            <div class="col">
+                <a href="/meetings">&lt; Back</a>
+            </div>
+        </div>
 
-    <h1>{{$meeting->title}}</h1>
+        <div class="row mb-4">
+            <div class="col">
+                <div class="mb-2">
+                    <h1>{{$meeting->title}}</h1>
+                </div>
 
-    <div>{{$meeting->description}}</div>
-    <div>{{$meeting->location}}</div>
-    <div>{{$meeting->start->format('Y M d @ H:i')}} - {{$meeting->end->format('Y M d @ H:i')}}</div>
+                <div class="meeting-meta">
+                    <div>
+                        {{$meeting->location}}
+                    </div>
 
-    <!-- here is where a form allowing users to toggle on/off the public meeting page - you will need to handle that form inside of a controller -->
+                    <div>
+                        {{$meeting->start->format('Y M d @ H:i')}} - {{$meeting->end->format('Y M d @ H:i')}}
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <h2>RSVPs</h2>
-    <!-- here is where a list of emails with their RSVP status for this meeting will go -->
+        <div class="row">
+            <div class="col-md-12">
+                <div>{{$meeting->description}}</div>
+
+                <!-- here is where a form allowing users to toggle on/off the public meeting page - you will need to handle that form inside of a controller -->
+
+                <div class="mt-5">
+                    <h2>RSVPs</h2>
+                    <!-- here is where a list of emails with their RSVP status for this meeting will go -->
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
